05057a482ce78503a8719b1e9f67aaaf916d03af
amolkpatil22 <amolkpatil22@gmail.com>
2024-12-18 00:36:15 +0530

    secure cookies

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -37,7 +37,7 @@ export class AuthService {
 
       res.cookie('accessToken', `Bearer ${accessToken}`, {
         httpOnly: true,
-        // secure: true,
+        secure: false,
         sameSite:"none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
       });
