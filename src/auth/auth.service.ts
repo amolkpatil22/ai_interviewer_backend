136ec86d2081a617a38774127aa44ed259c9a5f0
amolkpatil22 <amolkpatil22@gmail.com>
2024-12-18 00:27:23 +0530

    same site none

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -38,6 +38,7 @@ export class AuthService {
       res.cookie('accessToken', `Bearer ${accessToken}`, {
         httpOnly: true,
         // secure: true,
+        sameSite:"none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
       });
 
